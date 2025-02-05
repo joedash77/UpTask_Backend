1509f70c336c69194b3871c4e172ef8330d7f715
Joedash <joedash77@gmail.com>
2025-02-05 15:45:12 -0300

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,12 +5,12 @@ export const corsConfig : CorsOptions = {
         const whitelist = [process.env.FRONTEND_URL]
 
         if(process.argv[2] === '--api') {
-            whitelist.push(undefined)
+            whitelist.push(undefined) // Permitir solicitudes sin origen
         }
-        if(whitelist.includes(origin)) {
-            callback(null, true)
+        if(!origin || whitelist.includes(origin)) {
+            callback(null, true) // Permitir acceso
         } else {
-            callback(new Error('Error de CORS'))
+            callback(new Error('Error de CORS: Origen no permitido'))
         }
     }
 }
