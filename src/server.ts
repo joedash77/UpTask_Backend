1edb39de5112d8b6880bf66cbcad742f8e2bd644
Joedash <joedash77@gmail.com>
2025-02-05 17:53:14 -0300

    hecho

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,6 @@ import projectRoutes from './routes/projectRoutes'
 
 dotenv.config()
 
-console.log('Frontend URL desde Render:', process.env.FRONTEND_URL || 'No está definida');
 connectDB()
 
 const app = express()
@@ -20,15 +19,6 @@ app.use(morgan('dev'))
 
 // Leer datos del formulario
 app.use(express.json())
-
-app.get('/api/test-cors', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL || '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    
-    res.json({ message: 'CORS funcionando', frontend: process.env.FRONTEND_URL });
-});
 
 //Routes
 app.use('/api/auth', authRoutes)
